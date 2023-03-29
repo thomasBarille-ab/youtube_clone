45dfcbe52ae98bdcae50518e4f5cdcf634099a2e
thomasBarille-ab <thomasbarille@gmail.com>
2023-03-29 20:30:50 +0200

    debut projet

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+import { default as SearchBar } from "./SearchBar";
+import { default as Sidebar } from "./Sidebar";
